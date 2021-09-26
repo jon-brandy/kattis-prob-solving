63b93e07a75ffac119723cc83a4fc6a714d05036
Nicolas Saputra Gunawan <70703371+Brandy969@users.noreply.github.com>
2021-09-26 12:19:56 +0700

    Create solving-for-carrots.cpp

---
+++ solving-for-carrots.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N, P;
+    scanf("%d %d", &N, &P);
+    for(int i = 1; i <= N; i++)
+    {
+        char string[255];
+        scanf("%s", string);
+    }
+    printf("%d\n", P);
+    return 0;
+}
